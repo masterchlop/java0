640b8bc7e01a148daae749e76e54084c75e002d0
masterchlop <254661@student.pwr.edu.pl>
2020-10-15 10:47:06 +0200

    troche poprawione=f

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,7 +11,7 @@ public final class App   {
     /**
     @funkcja pisze zart i wita system
      */
-    int x=2332323ffdhd;
+    int x=233232333;
         public static void main(final String[] args){
         final String przywitanie = Klasa2.systemos();
         final String informacja = Klasa3.powitanie();
